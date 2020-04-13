f1f84c3e82bc3f98673c6b9b88c39b6f477b0354
LymarEvgeniy <zlymar@gmail.com>
2020-04-13 16:38:38 +0400

    6.5.2. Поиск минимума в диапазоне.[#265461]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -32,13 +32,13 @@ public class FindLoop {
      */
     public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
-       // for (int index = 0; index < data.length; index++) {
-            for (int index = start; index <= finish; index++) {
-                if (data[index] == el) {
-                    rst = index;
+        for (int index = 0; index < data.length; index++) {
+            for (int j = start; j <= finish; j++) {
+                if (data[j] == el) {
+                    rst = j;
                 }
             }
-       // }
+        }
         return rst;
     }
 }
