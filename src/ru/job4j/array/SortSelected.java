0dd455bbd0bfa53f7a76cb82563bfd26b2696d86
LymarEvgeniy <zlymar@gmail.com>
2020-04-13 17:07:47 +0400

    6.5.3. Сортировка выборкой.[#265459]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -1,17 +1,25 @@
-    package ru.job4j.array;
+/**
+ * SortSelected.
+ * @autor Evgeniy Lymar.
+ */
+package ru.job4j.array;
 
-    public class SortSelected {
-
-        public static int[] sort(int[] data) {
-            for (int i = 0; i < data.length; i++) {
-                int min = MinDiapason.findMin(data, i, data.length - 1);
-                int index = FindLoop.indexOf(data, min, i, data.length - 1);
-                if (data[i] > min) {
-                    int temp = data[index];
-                    data[index] = data[i];
-                    data[i] = temp;
-                }
+public class SortSelected {
+    /**
+     * Method sort.
+     * @param data array elements.
+     * @return return sorted elements.
+     */
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexOf(data, min, i, data.length - 1);
+            if (data[i] > min) {
+                int temp = data[index];
+                data[index] = data[i];
+                data[i] = temp;
             }
-            return data;
         }
+            return data;
     }
+}
--- test/ru/job4j/array/SortSelectedTest.java
+++ test/ru/job4j/array/SortSelectedTest.java
@@ -12,6 +12,7 @@ public class SortSelectedTest {
         int[] expect = new int[] {1, 2, 3, 4, 5};
         assertThat(result, is(expect));
     }
+
     @Test
     public void whenSortTree() {
         int[] input = new int[] {1, 5, 2};
