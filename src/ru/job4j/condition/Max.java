2a20d4fba3dacdf66870427f73b8f43795e6e5f5
LymarEvgeniy <zlymar@gmail.com>
2020-04-26 20:27:31 +0400

    3. Перегрузить метод max для трех чисел [#265606]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,8 +1,48 @@
 package ru.job4j.condition;
 
+/**
+ * Maximum variable values.
+ *
+ * @author Evgeniy Lymar.
+ */
 public class Max {
-    public static int max(int left, int right) {
-        int result = left >= right ? left : right;
-        return result;
+    /**
+     * Method max calculates the maximum values of two variable.
+     *
+     * @param left  value of the first variable.
+     * @param right value of the second variable.
+     * @return calculation result.
+     */
+    public int max(int left, int right) {
+        return Math.max(left, right);
+    }
+
+    /**
+     * Method max calculates the maximum values of three variable.
+     *
+     * @param first  value of the first variable.
+     * @param second value of the second variable.
+     * @param third  value of the third variable.
+     * @return calculation result.
+     */
+    public int max(int first, int second, int third) {
+        return max(first,
+                max(second, third)
+        );
+    }
+
+    /**
+     * Method max calculates the maximum values of four variable.
+     *
+     * @param first  value of the first variable.
+     * @param second value of the second variable.
+     * @param third  value of the third variable.
+     * @param fourth value of the fourth variable.
+     * @return calculation result.
+     */
+    public int max(int first, int second, int third, int fourth) {
+        return max(first,
+                max(second, third, fourth)
+        );
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -7,20 +8,57 @@ import static org.junit.Assert.assertThat;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int result = Max.max(1, 2);
+        Max check = new Max();
+        int result = check.max(1, 2);
         assertThat(result, is(2));
     }
 
     @Test
-    public void whenMax2To1Then2() {
-        int result = Max.max(3, 2);
+    public void whenMax2To3Then3() {
+        Max check = new Max();
+        int result = check.max(3, 2);
         assertThat(result, is(3));
     }
 
     @Test
-    public void whenEquallyThen2() {
-        int result = Max.max(3, 3);
+    public void whenEqually() {
+        Max check = new Max();
+        int result = check.max(3, 3);
         assertThat(result, is(3));
 
     }
+
+    @Test
+    public void whenSecondMax() {
+        Max check = new Max();
+        int result = check.max(1, 4, 2);
+        assertThat(result, Matchers.is(4));
     }
+
+    @Test
+    public void whenFirstMax() {
+        Max check = new Max();
+        int result = check.max(5, 4, 2);
+        assertThat(result, Matchers.is(5));
+    }
+
+    @Test
+    public void whenThirdMax() {
+        Max check = new Max();
+        int result = check.max(1, 4, 6);
+        assertThat(result, Matchers.is(6));
+    }
+
+    @Test
+    public void whenEqual() {
+        Max check = new Max();
+        int result = check.max(5, 5, 5);
+        assertThat(result, Matchers.is(5));
+    }
+    @Test
+    public void whenFourthMax() {
+        Max check = new Max();
+        int result = check.max(4, 2, 5, 9);
+        assertThat(result, Matchers.is(9));
+    }
+}
