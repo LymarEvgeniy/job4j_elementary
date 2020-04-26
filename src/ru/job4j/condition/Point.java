69423a6f2dad195a193578588935308f2cacfacc
LymarEvgeniy <zlymar@gmail.com>
2020-04-26 15:27:51 +0400

    2.1. Рефакторинг - Расстояние между точками. [#265603]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,18 +1,47 @@
 package ru.job4j.condition;
 
-public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double result = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return result;
-    }
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
-    public static void main(String[] args) {
-        double one = Point.distance(0, 0, 2, 0);
-        double two = Point.distance(1, 2, 4, 3);
-        double three = Point.distance(3, 5, 7, 4);
-        System.out.println("one (0, 0) to (2, 0) " + one);
-        System.out.println("two (1, 2) to (4, 3) " + two);
-        System.out.println("three (3, 5) to (7, 4) " + three);
+/**
+ * Point. Describes a point in a coordinate system.
+ * @author Evgeniy lymar.
+ */
+public class Point {
+    /**
+     * object field "x".
+     */
+    private  int x;
+    /**
+     * object field "y".
+     */
+    private  int y;
+    /**
+     * initial state constructor
+     * @param first initial state "x".
+     * @param second initial state "y".
+     */
+     public Point(int first, int second) {
+         this.x = first;
+         this.y = second;
+     }
+    /**
+     * Method distance takes a variable Point.
+     * @param that the variable with which we get the value.
+     * @return calculation result.
+     */
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
+    /**
+     * Method main. Demonstration of the exchange method.
+     * @param args args
+     */
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
+     }
 }
 
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -6,32 +6,26 @@ import org.junit.Test;
 public class PointTest {
     @Test
     public void whenDistance0020() {
-        int inX1 = 0;
-        int inY1 = 0;
-        int inX2 = 2;
-        int inY2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
         double expected = 2.0;
-        double out = Point.distance(inX1, inY1, inX2, inY2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
     @Test
     public void whenDistance1243() {
-        int inX1 = 1;
-        int inY1 = 2;
-        int inX2 = 4;
-        int inY2 = 3;
+        Point a = new Point(1, 2);
+        Point b = new Point(4, 3);
         double expected = 3.16;
-        double out = Point.distance(inX1, inY1, inX2, inY2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
     @Test
     public void whenDistance3574() {
-        int inX1 = 3;
-        int inY1 = 5;
-        int inX2 = 7;
-        int inY2 = 4;
+        Point a = new Point(3, 5);
+        Point b = new Point(7, 4);
         double expected = 4.12;
-        double out = Point.distance(inX1, inY1, inX2, inY2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
